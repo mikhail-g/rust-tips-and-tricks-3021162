017f3ccc9a8c1006ce16b81311fc41eaee4917f5
Misha Hariachyi <dev.Hariachyi.Misha@dev.bwater.com>
2024-01-19 11:21:18 +0100

    env! and option_env!

--- 01_04/environment_variables/src/main.rs
+++ 01_04/environment_variables/src/main.rs
@@ -10,8 +10,12 @@ fn third_party_api(username: &str, password: &str) -> String {
 }
 
 fn main() {
-    let username = "Elton".to_string();
-    let password = "my_password".to_string();
+    // let username = "Elton".to_string();
+    // let password = "my_password".to_string();
+    let username = env!("USERNAME");
+    // let username = option_env!("USERNAME").expect("'USERNAME' environment variable should be set!");
+    let password = option_env!("PASSWORD").expect("'PASSWORD' environment variable should be set!");
+    println!("username: '{}', password: '{}'", &username, &password);
 
     println!("Access Token: {}", third_party_api(&username, &password))
 }
