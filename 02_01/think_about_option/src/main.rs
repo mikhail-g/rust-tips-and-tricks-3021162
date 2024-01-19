020904d25b39a2874cff3255ce66ed3b085e721a
Misha Hariachyi <dev.Hariachyi.Misha@dev.bwater.com>
2024-01-19 11:33:47 +0100

    option

--- 02_01/think_about_option/src/main.rs
+++ 02_01/think_about_option/src/main.rs
@@ -2,11 +2,11 @@
 struct Invitation {
     invitee: String,
     attending: bool,
-    message: String,
+    message: Option<String>,
 }
 
 impl Invitation {
-    fn new(invitee: String, attending: bool, message: String) -> Invitation {
+    fn new(invitee: String, attending: bool, message: Option<String>) -> Invitation {
         Invitation {
             invitee,
             attending,
@@ -16,5 +16,11 @@ impl Invitation {
 }
 
 fn main() {
+    let inv1 = Invitation::new("Greg".to_string(), true, None);
+    let inv2 = Invitation::new("Yoi".to_string(), false, Some("Busy".to_string()));
+    dbg!(&inv1);
+    dbg!(&inv2);
 
+    println!("{:#?}", inv1);
+    println!("{:#?}", inv2);
 }
