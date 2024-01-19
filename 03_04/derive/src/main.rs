5eccd40a9f46394ae3828b6f3ee76f8eb6de6962
Misha Hariachyi <dev.Hariachyi.Misha@dev.bwater.com>
2024-01-19 12:25:39 +0100

    derive

--- 03_04/derive/src/main.rs
+++ 03_04/derive/src/main.rs
@@ -6,8 +6,13 @@ struct Person {
 
 impl Person {
     fn new(name: String, age: i32) -> Self {
-        Self {name, age}
+        Self { name, age }
     }
 }
 
-fn main() {}
+fn main() {
+    let person_1 = Person::new("B".to_string(), 2);
+    let person_2 = Person::new("A".to_string(), 3);
+
+    dbg!(person_2 > person_1);
+}
